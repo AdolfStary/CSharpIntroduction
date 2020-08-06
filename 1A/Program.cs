a86e07a35f4693e271ef5ff62fd9c6bb09b62b4d
AdolfStary <adolf.stary@hotmail.com>
2020-08-05 22:07:30 -0600

    Wrote the 1A practice program in C#, updated readme

---
+++ 1A/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string inputAsString = null;
+            int inputInt, result = 0;
+
+            // Asks for number
+            Console.Write("Hello! Input an integer to get it cubed: ");
+            
+            do 
+            {
+                // If given input is not an integer it will ask again
+                if(inputAsString != null)
+                {
+                    Console.Write("Not an integer, please enter an integer: ");
+                }
+
+                // Saves input as string first
+                inputAsString = Console.ReadLine();
+
+            }
+            // Tries to Parse the string into Int, if it fails, repeats the cycle
+            while (!Int32.TryParse(inputAsString, out inputInt)) ;
+
+            // Cubes the input
+            result = Convert.ToInt32(Math.Pow(inputInt, 3));
+
+            // Gives the solution to user and waits for input to close the program.
+            Console.WriteLine(inputInt + " cubed is " + result + ". Press enter to close the program...") ;
+            Console.ReadKey();
+
+        }
+
+
+        
+    }
+}
---
+++ 1A/README.md
@@ -0,0 +1,12 @@
+Requirements:
+Write a program that prompts the user for a number (as an int) and outputs the cube of that number (raised to the third power).
+
+
+Encountered bugs:
+Used method Convert.ToInt32(Console.Read()), which was giving a wrong number, Console.Read() reads the following character in ANSCII. I used Console.ReadLine() to fix the bug.
+
+
+
+Resources:
+https://www.c-sharpcorner.com/code/3836/how-to-calculate-cube-root-in-c-sharp.aspx
+https://stackoverflow.com/questions/24443827/reading-an-integer-from-user-input
