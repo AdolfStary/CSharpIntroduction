f7387e7632a1cd907d100ae638767a88c6f27f54
AdolfStary <adolf.stary@hotmail.com>
2020-08-08 22:17:29 -0600

    Completed 2C - Buckets of paint, updated readme

---
+++ 2C/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _2C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string amountString = null;
+            int amountPosts;
+            int amountOfPaint;
+            string bucket = " buckets ";
+
+            // How many posts can one bucket paint
+            int bucketVolume = 4;
+            
+
+            // Keeps asking for number input if input is wrong.
+            do
+            {
+                if (amountString != null) Console.Write("Wrong input, please input a number. ");
+                Console.Write("How many fence posts do we have to paint? : ");
+                amountString = Console.ReadLine();
+            }
+            while (!int.TryParse(amountString, out amountPosts));
+
+            // Calculates how many buckets we need
+            amountOfPaint = amountPosts / bucketVolume;
+            // If there is leftover after dividing number of posts by bucket's volue it will add one more bucket of paint to the total
+            if (amountPosts % bucketVolume > 0) amountOfPaint++;
+            // If we only need one bucket, it changes buckets into bucket, to make it bit more polished.
+            if (amountOfPaint == 1) bucket = " bucket ";
+            
+            // Outputs result and prompts user to press enter to exit the program.
+            Console.Write("We will need " + amountOfPaint + bucket + "of paint. \nPress enter to exit the program...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 2C/README.md
@@ -0,0 +1,9 @@
+Requirements:
+Write a program that will take in a number of fence posts and determine how many buckets of paint will be required to paint the fence posts. One bucket paints 4 fence posts.
+
+
+Encountered bugs:
+
+
+Resources:
+Previous projects
