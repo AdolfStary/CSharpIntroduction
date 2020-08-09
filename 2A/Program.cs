4e308240f76b6105c03a4114e8d16edfb154faba
AdolfStary <adolf.stary@hotmail.com>
2020-08-08 20:13:33 -0600

    Completed practice project 2A - Uppercase

---
+++ 2A/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputString;
+
+            // Asks for input
+            Console.Write("Enter a string to be converted into uppercase: ");
+            inputString = Console.ReadLine();
+
+
+            // Converts string into uppercase
+            inputString = inputString.ToUpper();
+
+
+            // Returns result and awaits action to exit program
+            Console.WriteLine("Your input in Uppercase: " + inputString);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 2A/README.md
@@ -0,0 +1,11 @@
+Requirements:
+Write a program that will take in a string, convert it to uppercase, and output it.
+
+
+Encountered bugs:
+- It wouldn't work at first, then I realized I didn't reassign the variable value when performing .ToUpper(), inputString = inputString.ToUpper(); fixed the issue
+
+
+
+Resources:
+https://www.geeksforgeeks.org/c-sharp-toupper-method/#:~:text=In%20C%23%2C%20ToUpper%20%28%29%20is%20a%20string%20method.,by%20passing%20different%20type%20of%20arguments%20to%20it.
